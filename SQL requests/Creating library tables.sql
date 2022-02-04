6b20e3cd51f3d4ffa0cf90fa83bcf7a7573d3172
Artem <temach2002@gmail.com>
2022-02-04 15:40:35 +0300

    Updated formatting

--- SQL requests/Creating library tables.sql
+++ SQL requests/Creating library tables.sql
@@ -1,29 +1,29 @@
 create database library;
 
 CREATE TABLE categories (
-	category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
     category_name VARCHAR(70) NOT NULL
 );
 
 CREATE TABLE authors (
-	author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
     patronymic VARCHAR(50)
 );
 
 CREATE TABLE books (
-	book_id INT PRIMARY KEY AUTO_INCREMENT,
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(70) NOT NULL,
     author_id INT NOT NULL,
     stock INT NOT NULL DEFAULT 0,
     category_id INT NOT NULL,
-	FOREIGN KEY (author_id) REFERENCES authors (author_id),
-	FOREIGN KEY (category_id) REFERENCES categories (category_id)
+    FOREIGN KEY (author_id) REFERENCES authors (author_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id)
 );
 
 CREATE TABLE users (
-	user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
     patronymic VARCHAR(50),
@@ -34,7 +34,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE orders (
-	order_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    order_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     book_id INT NOT NULL,
     start_date DATE NOT NULL,
