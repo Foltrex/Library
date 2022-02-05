35f79da6cd34c37aa3f735fc7cb9ce56daee6123
Artem <temach2002@gmail.com>
2022-02-05 18:29:39 +0300

    SQL Modified

--- SQL requests/Creating library tables.sql
+++ SQL requests/Creating library tables.sql
@@ -2,44 +2,46 @@ CREATE DATABASE library;
 USE library;
 
 CREATE TABLE genres (
-    genre_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(70) NOT NULL
 );
 
 CREATE TABLE authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
     patronymic VARCHAR(50)
 );
 
 CREATE TABLE books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(70) NOT NULL,
     author_id INT NOT NULL,
     stock INT NOT NULL DEFAULT 0,
     category_id INT NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES authors (author_id),
-    FOREIGN KEY (genre_id) REFERENCES genres (genre_id)
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id)
 );
 
 CREATE TABLE users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
     patronymic VARCHAR(50),
-    number VARCHAR(32) NOT NULL,
+    subscription_number VARCHAR(32) NOT NULL,
+    phone_number VARCHAR(32) NOT NULL,
     login VARCHAR(50) NOT NULL,
     password VARCHAR(32) NOT NULL,
     role enum ('admin', 'librarian', 'reader') NOT NULL
 );
 
-CREATE TABLE orders (
-    order_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE rentals (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     book_id INT NOT NULL,
-    start_date DATE NOT NULL,
+    start_date DATE,
     end_date DATE,
-    FOREIGN KEY (book_id) REFERENCES books (book_id),
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    status enum ('waiting for issuance', 'issued to the reading room', 'issued for subscription', 'returned') NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES books (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
--- src/main/java/com/epam/library/entity/User.java
+++ src/main/java/com/epam/library/entity/User.java
@@ -4,21 +4,37 @@ public class User {
     private int id;
     private String name;
     private String surname;
-    private String patronymic;
-    private String number;
+    private String phoneNumber;
     private String login;
     private String password;
     private Role role;
+    private String subscriptionNumber;
 
-    public User(int id, String name, String surname, String patronymic, String number, String login, String password, Role role) {
+    public User(int id, String name, String surname, String phoneNumber, String login, String password, Role role, String subscriptionNumber) {
         this.id = id;
         this.name = name;
         this.surname = surname;
-        this.patronymic = patronymic;
-        this.number = number;
+        this.phoneNumber = phoneNumber;
         this.login = login;
         this.password = password;
         this.role = role;
+        this.subscriptionNumber = subscriptionNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSubscriptionNumber() {
+        return subscriptionNumber;
+    }
+
+    public void setSubscriptionNumber(String subscriptionNumber) {
+        this.subscriptionNumber = subscriptionNumber;
     }
 
     public int getId() {
@@ -43,22 +59,6 @@ public class User {
 
     public void setSurname(String surname) {
         this.surname = surname;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
     }
 
     public String getLogin() {
